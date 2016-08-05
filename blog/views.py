83802add7a9c275ef98e205b45d855a40e05b2dc
bablisoktanya <bablisok.tanya@gmail.com>
2016-08-05 16:47:39 +0300

    Site için HTML dosyasını değiştirdim.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from django.utils import timezone
-from .models import Post
 
-def post_list(request):
-    posts = Post.objects.order_by('yayinlama_tarihi')
-    return render(request, 'blog/post_list.html', {"posts": posts})
+def post_list(request):     
+    return render(request, 'blog/post_list.html', {})
